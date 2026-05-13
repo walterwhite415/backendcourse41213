bb056ce3679a36be8c044c34dbc5670659804311
walterwhite415 <abc07n@gmail.com>
2026-05-13 21:40:00 +0300

    загрузка работы

---
+++ index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="ru">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="normalize.css">
+    <link rel="stylesheet" href="main.css">
+    <title>lab1</title>
+  </head>
+  <body>
+    <header>
+      <div class="header-wr">
+        <a href="https://mospolytech.ru/">
+          <picture >
+            <img class="logo" src="img/logo.jpg" alt="Лого" />
+          </picture>
+        </a>
+        <h1>Домашняя работа: Hello, World!</h1>
+      </div>
+    </header>
+    <main>
+      <section>
+        <div class="wr">
+          <p><?='Hello, world!'?></p>
+        </div>
+      </section>
+    </main>
+    <footer>
+      <div class="wr">задание для самостоятельной работы</div></footer>
+  </body>
+</html>
